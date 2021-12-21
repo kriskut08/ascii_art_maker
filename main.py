f503d2a6bf5a43567248a986a5cb25541498bd6a
kriskut08 <73740605+kriskut08@users.noreply.github.com>
2021-12-21 21:47:12 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from PIL import Image, ImageOps
+from tkinter import *
+from tkinter import filedialog
+from tkinter import ttk
+from tkinter.messagebox import showinfo
+from PIL import Image,ImageOps
+
+
 
 """
 Képből ascii art generátor
@@ -12,34 +18,75 @@ qkristof.hu
 
 
 
-width = 100
-height = 100
+win = Tk()
 
-#a karakterek
-shades = "$@%#*/\|()1{}[]?o<>i!lI-_+~;:,"
+#fájlok bekérése
+def openImage():
+    global inp
+    global open_btn
+    inp = filedialog.askopenfilename(title="Fájlok kiválasztása",
+                                          filetypes=(("Kép ",
+                                                      "*.jpg*"),("Kép","*.png*"),
+                                                     ("all files",
+                                                      "*.*")))
+    name = inp.split("/")
+    open_btn.configure(text=name[-1])
 
-#a kép megnyitása, átméterezése, forgatása
-im = Image.open("test.jpg")
-im = im.convert("P", palette=Image.ADAPTIVE, colors=64)
-im = im.resize((width,height))
-im = ImageOps.grayscale(im)
-im = im.rotate(90)
-im = ImageOps.flip(im)
-
-
+def openoutput():
+    global output
+    global output_btn
+    output =filedialog.asksaveasfilename(initialdir="/",
+                                          title="Select a File",
+                                          filetypes=(("Text files",
+                                                      "*.txt*"),
+                                                     ("all files",
+                                                      "*.*")))
+    name = output.split("/")
+    output_btn.configure(text=name[-1])
 
 #a kép konvertálása
-text = ""
-for i in range(width):
-    for j in range(height):
-        text += shades[int(im.getpixel((i, j)) / 8.6)]
-        text += shades[int(im.getpixel((i, j)) / 8.6)]
-    text += "\n"
+def convertImage(img, out):
+    width = 100
+    height = 100
+
+    # a karakterek
+    shades = "$@%#*/\|()1{}[]?o<>i!lI-_+~;:,"
+
+    # a kép megnyitása, átméterezése, forgatása
+    im = Image.open(img)
+    im = im.convert("P", palette=Image.ADAPTIVE, colors=64)
+    im = im.resize((width, height))
+    im = ImageOps.grayscale(im)
+    im = im.rotate(90)
+    im = ImageOps.flip(im)
+
+    text = ""
+    for i in range(width):
+        for j in range(height):
+            text += shades[int(im.getpixel((i, j)) / 8.6)]
+            text += shades[int(im.getpixel((i, j)) / 8.6)]
+        text += "\n"
+
+    # a "kép" fájlba írása
+    out = open(out, "w+")
+    out.write(text)
+    out.close()
+    showinfo("Kész!","A konvertálás sikeres volt!")
 
 
-# a "kép" fájlba írása
-out = open("output.txt","w+")
-out.write(text)
-out.close()
+win.geometry("400x268")
+win.title("Kép-ascii konverter")
+win.resizable(False, False)
+output=""
+inp=""
+
+open_btn = Button(win, text="Kép kiválasztása", command = lambda: openImage())
+open_btn.pack()
+output_btn = Button(win,text="Kimeneti fájl megnyitása",command = lambda: openoutput())
+output_btn.pack()
+convert =Button(win,text="Konvertálás",command = lambda: convertImage(inp,output)).pack()
 
 
+
+
+win.mainloop()
